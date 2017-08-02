d2bd9f4687a66d68e7978f8ba0b813c4e461065f
Tristan Schmitt <trschmitt50@live.com>
2017-08-02 09:39:29 -0400

    added unemployed and employed framework for mongo to be ready to edit when using mongoose

--- models/user.js
+++ models/user.js
@@ -14,9 +14,24 @@ function findById(id, callback) {
   profiles.findOne({"id": id}, (err, data) => {
     callback(data);
   });
-
 }
-///////end of mongo code
+
+//////edit these properly//////
+function findAllEmployed(callback) {
+  let profiles = client.db.collection("profiles");
+  profiles.find({"job": {$ne: null}}.toArray((err, data) => {
+    callback(data);
+  });
+}
+
+function finAllUnemployed(callback) {
+  let profiles = client.db.collection("profiles");
+  profiles.find({"job": null}.toArray((err, data) => {
+    callback(data);
+  });
+}
+/////////////////////////
+///////end of mongo code/////////
 
 module.exports = {
   findAll: findAll,
